0135a0f3afbfd8e45b9a4fbabd6d551ada3ad31b
Dmitriy Hladyshev <dmglad7@gmail.com>
2018-10-09 15:45:56 +0300

    errors messages added

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -28,34 +28,55 @@ class PostController extends Controller
             'title' => $request->title,
             'body' => $request->body,
         ]);
-        return 'New post created';
+        //return 'New post created';
+        return response()->json([
+            'message' => 'New post created',
+        ], 200);
     }
 
     public function posts_delete(Request $request, $id) {
         $postCheck = Post::where('id', $id)
             ->where('user_id', $request->user_id)
             ->first();
-        if(!$postCheck) dd('Post NOT exists');
+        if(!$postCheck)
+            return response()->json([
+                'message' => 'Post NOT exists',
+            ], 400);
+            //dd('Post NOT exists');
 
         Post::where('id', $id)
         ->where('user_id', $request->user_id)
         ->delete();
-        return 'Post with id = '.$id.' deleted';
+        //return 'Post with id = '.$id.' deleted';
+        return response()->json([
+            'message' => 'Post with id = '.$id.' deleted',
+        ], 200);
     }
 
     public function posts_update(Request $request, $id) {
         $postCheck = Post::where('id', $id)
             ->where('user_id', $request->user_id)
             ->first();
-        if(!$postCheck) dd('Post NOT exists');
+        if(!$postCheck)
+            return response()->json([
+                'message' => 'Post NOT exists',
+            ], 400);
+
+            //dd('Post NOT exists');
 
         Post::find($id)->update($request->all());
-        return 'Post with id = '.$id.' updated';
+        //return 'Post with id = '.$id.' updated';
+        return response()->json([
+            'message' => 'Post with id = '.$id.' updated',
+        ], 200);
     }
 
     public function one_post(Request $request, $id) {
         $onePost = Post::where('id', $id)
             ->first();
+        if(is_null($onePost)) return response()->json([
+            'message' => 'Record not found',
+        ], 400);
         return $onePost;
     }
 
